bec733f0dcdc93b5a96e597f2a4d7b2804faa873
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-07-03 10:05:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,26 +2,27 @@ const progress = document.getElementById('progress');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 const circles = document.querySelectorAll('.circle');
+const lines = document.querySelectorAll('.line');
 
 let currentActive = 1;
 
 next.addEventListener('click', () => {
     currentActive++;
-    
+
     if (currentActive > circles.length) {
         currentActive = circles.length;
     }
-    
+
     update();
 });
 
 prev.addEventListener('click', () => {
     currentActive--;
-    
+
     if (currentActive < 1) {
         currentActive = 1;
     }
-    
+
     update();
 });
 
@@ -34,10 +35,18 @@ function update() {
         }
     });
 
-    const actives = document.querySelectorAll('.active');
-    
+    lines.forEach((line, idx) => {
+        if (idx < currentActive - 1) {
+            line.classList.add('active');
+        } else {
+            line.classList.remove('active');
+        }
+    });
+
+    const actives = document.querySelectorAll('.circle.active');
+
     progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
-    
+
     if (currentActive === 1) {
         prev.disabled = true;
     } else if (currentActive === circles.length) {
